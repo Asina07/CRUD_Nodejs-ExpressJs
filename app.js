52d26b448b3ba85742ac6dcac9c890129cf88122
Asina07 <asinaabdulla1111@gmail.com>
2022-05-26 11:55:20 +0400

    CRUD File

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//-------------------------db connction---------------------------
+const mongoose = require ('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/Employee_Data').then(()=>{
+    console.log('Employee Data connected')
+}).catch(err => console.log(err));
+
+//---------------------db connected--------------------------------
+
+
+
+//----------------------import Router-------------------------------
+
+const EmployeeRouter = require('./routes/employee_router') 
+app.use('/Employee',EmployeeRouter)
+
+// { connect mongoDb from -----> 'ThisPc /c: /ProgramFile /MongoDb /Server /5.0 /bin' ------->cmd --> enter mongo}
+
+//----------------------Router imported--------------------------
+module.exports = app;
